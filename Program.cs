d18d4941e0c8fb7d284d30b92b9972b63e67128e
thaynasantosss <thayna.2015.09@gmail.com>
2023-03-31 20:01:38 -0300

    Projeto Concluído

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string Nome, Sobrenome;
+Console.Write("Digite seu primeiro nome: ");
+Nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+Sobrenome = Console.ReadLine()!;
+
+string NomeCompleto = Nome + " " + Sobrenome;
+string NomeCatálogo = Sobrenome.ToUpper() + ", " + Nome;
+
+Console.WriteLine("Seu nome completo é:");
+Console.WriteLine(NomeCompleto);
+
+
+Console.WriteLine("Seu Nome de Catálogo é:"); 
+Console.WriteLine(NomeCatálogo);
